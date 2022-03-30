7bd522e44911ae088facf3aa5ce2cb4b116c20df
AlexCreem <47860604+AlexCreem@users.noreply.github.com>
2022-03-30 14:39:01 -0400

    Implemented Fizzbuzz

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -4,14 +4,33 @@ public class FizzBuzz
 {
     String processNumber(int num)
 	{	
-    	// Copy your code here
-		return "1";
+    	String result=Integer.toString(num);
+		if (num %3 ==0 && num %5 ==0) {
+			result = "Fizzbuzz";
+		}
+		else if (num%3 ==0) {
+			result = "Fizz";
+		}
+		else if (num%5 ==0) {
+			result = "Buzz";
+		}
+        return result;
 	}
 	
 	String processNumbers (int[] numList)
 	{
-    	// Copy your code here
-		return "";
+    	String result = "";
+        
+        for (int i= 0;i < nums.length; i++) {
+        	if (i != nums.length -1) {
+        		result += processNumber(nums[i]) + ", ";
+        	}
+        	else {
+        		result += processNumber(nums[i]);
+        	}
+        	
+        }
+        return result;
 	}
 
 }
